d8f7d0957895e80735f7b3becd410db1b17bdd50
Etens <45323562+Etens@users.noreply.github.com>
2023-01-31 11:46:54 +0200

    Add sound effet

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,16 +6,18 @@ import {
     faSignInAlt,
     faShip,
 } from "@fortawesome/free-solid-svg-icons";
-import "./Home.css";
 import SignUpForm from "../forms/SignUpForm";
 import LogInForm from "../forms/LogInForm";
 import { useNavigate, Link } from 'react-router-dom';
 import { useGlobalContext } from "../../contexts/GlobalContext";
+import AudioPlayer from "react-audio-player";
+
+import "./Home.css";
+
 export default function Home() {
     const navigate = useNavigate();
     const [zoom, setZoom] = useState(false);
     const [buttonClicked, setButtonClicked] = useState(false);
-
     const {
         showSignUpModal,
         showLogInModal,
@@ -32,15 +34,24 @@ export default function Home() {
         setTimeout(() => {
             navigate("/game");
         }, 500);
+        // return (
+        //     <AudioPlayer src="https://cdn.pixabay.com/download/audio/2021/08/04/audio_dea21d9092.mp3?filename=game-start-6104.mp3" controls autoPlay />
+        // );
     };
     const handleLogin = () => {
         return (
-            <LogInForm />
+            <>
+                <LogInForm />
+                <AudioPlayer src="https://cdn.pixabay.com/download/audio/2022/09/29/audio_a4b3f2fe44.mp3?filename=select-sound-121244.mp3" autoPlay />
+            </>
         );
     };
     const handleSignUp = () => {
         return (
-            <SignUpForm />
+            <>
+                <SignUpForm />
+                <AudioPlayer src="https://cdn.pixabay.com/download/audio/2022/03/15/audio_23b7958390.mp3?filename=menu-click-89198.mp3" autoPlay />
+            </>
         );
     };
     const handleInfo = () => {
@@ -48,12 +59,13 @@ export default function Home() {
             <div className="screen-display">
                 <h1>INFO</h1>
                 {/* component info */}
+                <AudioPlayer src="https://cdn.pixabay.com/download/audio/2022/01/18/audio_a29a673ef4.mp3?filename=decidemp3-14575.mp3" autoPlay />
             </div>
         );
     };
+
     return (
         <div className="container">
-            {console.log("game")}
             <div className={`arcade-machine ${zoom ? "zoom" : ""}`}>
                 <div className="shadow"></div>
                 <div className="top">
